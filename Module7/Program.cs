7679c760324a9c0d4e6041d4c5fb8ff1467fe790
СантыбаевХ <@403_2_>
2017-06-30 21:51:18 +0600

    Add project files.

---
+++ Module7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+
+namespace Module7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tank> tankList = new List<Tank>();
+            tankList.Add(new Tank("T34"));
+            tankList.Add(new Tank("Pantera 1"));
+            tankList.Add(new Tank("Pantera 2"));
+            tankList.Add(new Tank("Pantera 3"));
+            tankList.Add(new Tank("Pantera 4"));
+            foreach (var item in tankList)
+            {
+                Console.WriteLine("Имя танка: {0}\t Боекомплект: {1}\t Броня: {2}\t Маневренность: {3}", item.getName(), item.getAmmo(), item.getArmor(), item.getManeuverability());
+            }
+            for(int i = 0; i < tankList.Count; i++)
+            {
+                for(int j = 0; j < tankList.Count; j++)
+                {
+                    if (tankList[i].isDead() && tankList[j].isDead() && i != j)
+                    {
+                        Tank tempTank = tankList[i] * tankList[j];
+                        Console.WriteLine("В битве {0} против {1} победил {2}", tankList[i], tankList[j], tempTank.getName());
+                    }
+                }
+            }
+            foreach (var item in tankList)
+            {
+                if (item.isDead())
+                    Console.WriteLine("Победитель битвы {0}",item.getName());
+            }
+
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+
+    public class Tank
+    {
+        private string name;
+        private int ammo;
+        private int armor;
+        private int maneuverability; //маневренность ёпт
+        private bool isAlive = true;
+
+        public Tank(string name)
+        {
+            this.name = name;
+            ammo = GetRandomNumber(0, 100);
+            armor = GetRandomNumber(0, 100);
+            maneuverability = GetRandomNumber(0, 100);
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public string getAmmo()
+        {
+            return ammo.ToString();
+        }
+        public string getArmor()
+        {
+            return armor.ToString();
+        }
+        public string getManeuverability()
+        {
+            return maneuverability.ToString();
+        }
+        public bool isDead()
+        {
+            return isAlive;
+        }
+        private static readonly Random getrandom = new Random();
+        private static readonly object syncLock = new object();
+        private static int GetRandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { // synchronize
+                return getrandom.Next(min, max);
+            }
+        }
+
+        public static Tank operator *(Tank tank1, Tank tank2)
+        {
+            int points1 = 0;
+            int points2 = 0;
+            if (tank1.ammo > tank2.ammo)
+                points1++;
+            else
+                points2++;
+            if (tank1.armor > tank2.armor)
+                points1++;
+            else
+                points2++;
+            if (tank1.maneuverability > tank2.maneuverability)
+                points1++;
+            else
+                points2++;
+            if (points1 > points2)
+            {
+                tank2.isAlive = false;
+                return tank1;
+            }
+            else
+            {
+                tank1.isAlive = false;
+                return tank2;
+            }
+        }
+
+    }
+}
